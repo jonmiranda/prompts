1eddb6cae5ddf11100a96fe9da80516a85e4c8da
Jon Miranda <miranda.jra@gmail.com>
2015-04-26 10:09:26 -0700

    Removed redundant code to PromptView.

--- app/src/main/java/net/jonmiranda/prompts/views/PromptView.java
+++ app/src/main/java/net/jonmiranda/prompts/views/PromptView.java
@@ -2,9 +2,9 @@ package net.jonmiranda.prompts.views;
 
 public interface PromptView {
 
-    public static final String PROMPT_KEY = "PROMPT_KEY";
-    public static final String COLOR_KEY = "COLOR_KEY";
-    public static final String DATE_KEY = "DATE_KEY";
+    String PROMPT_KEY = "PROMPT_KEY";
+    String COLOR_KEY = "COLOR_KEY";
+    String DATE_KEY = "DATE_KEY";
 
     void setColor(int color);
 
