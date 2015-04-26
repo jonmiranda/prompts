77d90e6968e4fa7affa52daac8c6d2c4b53547a0
Jon Miranda <miranda.jra@gmail.com>
2015-04-26 13:00:42 -0700

    Remove dead method: getRealmDateString.

--- app/src/main/java/net/jonmiranda/prompts/app/Utils.java
+++ app/src/main/java/net/jonmiranda/prompts/app/Utils.java
@@ -6,11 +6,6 @@ import java.util.GregorianCalendar;
 
 public class Utils {
 
-    public static String getRealmDateString(Calendar date) {
-        return String.format("%d-%d-%d",
-                date.get(Calendar.DATE), date.get(Calendar.MONTH), date.get(Calendar.YEAR));
-    }
-
     public static String getPrettyDateString(Calendar date) {
         return String.format("%1$tA %1$tB %1$te, %1$tY", date);
     }
