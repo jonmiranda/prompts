2378b1a99b45f8234cbe5ef0ab255bc6a610ed9f
Jon Miranda <miranda.jra@gmail.com>
2015-05-29 13:40:20 -0700

    Added test: testChangingDatePreservesResponses

--- app/src/androidTest/java/net/jonmiranda/prompts/MainActivityTest.java
+++ app/src/androidTest/java/net/jonmiranda/prompts/MainActivityTest.java
@@ -5,13 +5,20 @@ import android.test.suitebuilder.annotation.SmallTest;
 
 import net.jonmiranda.prompts.ui.main.MainActivity;
 
+import java.util.Calendar;
+
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.clearText;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.swipeLeft;
 import static android.support.test.espresso.action.ViewActions.swipeRight;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.allOf;
 
 @SmallTest
 public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
@@ -38,6 +45,58 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
             onView(withText(prompts[i])).check(matches(isDisplayed()));
             onView(withId(R.id.container)).perform(swipeRight());
         }
+    }
+
+    private static void clearResponseText() {
+        onView(allOf(withId(R.id.editor), isDisplayed())).perform(clearText());
+    }
+
+    private static void enterPromptResponse(String response) {
+        onView(allOf(withId(R.id.editor), isDisplayed())).perform(typeText(response));
+    }
+
+    private static void showPreviousDate() {
+        onView(withId(R.id.arrow_left)).perform(click());
+    }
+
+    private static void showNextDate() {
+        onView(withId(R.id.arrow_right)).perform(click());
+    }
+
+    public void testChangingDatePreservesResponses() {
+        Calendar today = Calendar.getInstance();
+        Calendar yesterday = Calendar.getInstance();
+        yesterday.add(Calendar.DATE, -1);
+
+        String todaysResponse = today.getTime().toLocaleString();
+        String yesterdaysResponse = yesterday.getTime().toLocaleString();
+
+        // Let's clear yesterday and today's response
+        clearResponseText();
+        showPreviousDate();
+        clearResponseText();
+
+        // Reset to today
+        showNextDate();
+
+        // Enter in today's response
+        enterPromptResponse(todaysResponse);
+
+        // Change date to yesterday
+        showPreviousDate();
+        onView(withText(todaysResponse)).check(doesNotExist());
+
+        // Set yesterday's response
+        enterPromptResponse(yesterdaysResponse);
+        onView(withText(yesterdaysResponse)).check(matches(isDisplayed()));
+
+        // Change date to today, Confirm persistence
+        showNextDate();
+        onView(withText(todaysResponse)).check(matches(isDisplayed()));
+
+        // Change date to yesterday, Confirm persistence
+        showPreviousDate();
+        onView(withText(yesterdaysResponse)).check(matches(isDisplayed()));
     }
 
 
